a07f8c7ac91379ae23a02233ee5fa77e5fc020a4
sdores <sdores23@hotmail.com>
2022-12-01 00:27:25 +0000

    Delete DockerAdult/ejemplo api-github-docker-20221129T133242Z-001/adult_api directory

--- DockerAdult/ejemplo api-github-docker-20221129T133242Z-001/adult_api/model/__init__.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-# Copyright 2022 Sergio Doreste Buerles
-# See LICENSE for details.
-# Author: Sergio Doreste Buerles
-
-
-__author__ = 'Sergio Doreste Buerles'
-__version__ = '1.0'
